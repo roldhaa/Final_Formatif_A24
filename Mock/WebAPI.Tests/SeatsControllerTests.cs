946fcbadc41c11750d2e89d90a48116934b43bdb
harold tcheuko <haroldtcheuko@gmail.com>
2025-11-27 18:04:32 -0500

    fct question 1 fini

--- Mock/WebAPI.Tests/SeatsControllerTests.cs
+++ Mock/WebAPI.Tests/SeatsControllerTests.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebAPI.Controllers;
+using WebAPI.Exceptions;
+using WebAPI.Models;
+using WebAPI.Services;
 
 namespace WebAPI.Tests;
 
@@ -7,8 +12,84 @@ namespace WebAPI.Tests;
 public class SeatsControllerTests
 {
     [TestMethod]
-    public void ReserveSeat()
+    public void ReserveSeat_ok()
     {
-        
+        Mock<SeatsService> serviceMock = new Mock<SeatsService>();
+        Mock<SeatsController> controller = new Mock<SeatsController>(serviceMock.Object) { CallBase = true };
+
+        Seat seat = new Seat();
+        seat.Id = 1;
+        seat.Number = 1;
+
+        serviceMock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Returns(seat);
+        controller.Setup(c => c.UserId).Returns("11111");
+
+        var actionResult = controller.Object.ReserveSeat(seat.Number);
+
+        var result = actionResult.Result as OkObjectResult;
+
+        Assert.IsNotNull(result);
+
+     
+
+    }
+
+    [TestMethod]
+    public void ReserveSeat_Unauthorized()
+    {
+        Mock<SeatsService> serviceMock = new Mock<SeatsService>();
+        Mock<SeatsController> controller = new Mock<SeatsController>(serviceMock.Object) { CallBase = true };
+
+
+        serviceMock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new SeatAlreadyTakenException());
+        controller.Setup(c => c.UserId).Returns("11111");
+
+        var actionResult = controller.Object.ReserveSeat(1);
+
+        var result = actionResult.Result as UnauthorizedResult;
+
+        Assert.IsNotNull(result);
+       
+
+    }
+
+    [TestMethod]
+    public void ReserveSeat_NotFound()
+    {
+        Mock<SeatsService> serviceMock = new Mock<SeatsService>();
+        Mock<SeatsController> controller = new Mock<SeatsController>(serviceMock.Object) { CallBase = true };
+
+
+        serviceMock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new SeatOutOfBoundsException());
+        controller.Setup(c => c.UserId).Returns("11111");
+
+        var seatNumber = 1;
+
+        var actionResult = controller.Object.ReserveSeat(seatNumber);
+
+        var result = actionResult.Result as NotFoundObjectResult;
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual("Could not find " + seatNumber, result.Value);
+
+    }
+    [TestMethod]
+    public void ReserveSeat_BadRequest()
+    {
+        Mock<SeatsService> serviceMock = new Mock<SeatsService>();
+        Mock<SeatsController> controller = new Mock<SeatsController>(serviceMock.Object) { CallBase = true };
+
+
+        serviceMock.Setup(s => s.ReserveSeat(It.IsAny<string>(), It.IsAny<int>())).Throws(new UserAlreadySeatedException());
+        controller.Setup(c => c.UserId).Returns("11111");
+
+        var actionResult = controller.Object.ReserveSeat(1);
+
+        var result = actionResult.Result as BadRequestResult;
+
+        Assert.IsNotNull(result);
+       
+
+
     }
 }
--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -1,16 +1,57 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component } from '@angular/core';
 import { bounce, shakeX, tada } from 'ng-animate';
+import { timer } from 'rxjs';
+import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
 
+
+const BOUNCE_DURATION_SECONDS = 2.0;
+const SHAKE_DURATION_SECONDS = 4.0;
+const TADA_DURATION_SECONDS = 3.0;
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: true
+    standalone: true,
+     animations: [
+    trigger('bounce', [transition(':increment', useAnimation(bounce, {params: {timing: BOUNCE_DURATION_SECONDS}}))]),
+    trigger('shake', [transition(':increment', useAnimation(shakeX, {params: {timing: SHAKE_DURATION_SECONDS}}))]),
+    trigger('tada', [transition(':increment', useAnimation(tada, {params: {timing: TADA_DURATION_SECONDS}}))]),
+  ]
 })
 export class AppComponent {
   title = 'ngAnimations';
 
+  ng_bounce = 0;
+  ng_shake = 0;
+  ng_tada = 0;
+
   constructor() {
+
   }
+  async waitFor(delayInSeconds:number) {
+  await lastValueFrom(timer(delayInSeconds * 1000));
+}
+
+async Animationunefois(){
+
+   this.ng_shake++
+
+  await lastValueFrom(timer(SHAKE_DURATION_SECONDS * 1000));
+  // Après 2 seconde
+  this.ng_bounce++;
+
+  await lastValueFrom(timer(BOUNCE_DURATION_SECONDS * 1000));
+  // Après 4 secondes
+this.ng_tada++
+  }
+
+  lancerAnimations(boucle: boolean) {
+  ... exécute les 3 animations ...
+  if (boucle === true) {
+     ... recommence la séquence ...
+  }
+}
+
+
 }
